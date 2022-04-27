03135c46f826bfb2594d9656e2d4cf3231ca1c90
harishra <ranjani.harish@foxtel.com.au>
2022-04-27 14:35:52 +1000

    More updates

--- src/main/java/com/reflectoring/library/web/LibraryController.java
+++ src/main/java/com/reflectoring/library/web/LibraryController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -31,8 +32,8 @@ public class LibraryController {
     }
 
     @GetMapping
-    public ResponseEntity<List<BookDto>> getBooks() {
-        return ResponseEntity.ok().body(libraryService.getAllBooks());
+    public ResponseEntity<List<BookDto>> getBooks(@RequestParam String type) {
+        return ResponseEntity.ok().body(libraryService.getAllBooks(type));
     }
 
     @PostMapping
